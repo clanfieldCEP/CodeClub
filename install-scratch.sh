6375d743e4e85cc855ff07241629d405bf4885ee
clanfieldCEP <head.3100@clanfield.oxon.sch.uk>
2015-03-05 19:45:20 +0000

    updated install-scratch.sh

--- install-scratch.sh
+++ install-scratch.sh
@@ -5,7 +5,7 @@
 # This setup script will create directories and download the Scratch files
 #
 # wget --content-disposition enos.in/scratch14
-# 
+#
 
 sudo mkdir /usr/share/scratch/Media/Resources
 sudo mkdir /usr/share/scratch/Media/ChristmasResources
@@ -14,5 +14,11 @@ sudo chown pi:pi /usr/share/scratch/Media/ChristmasResources
 cd ~/Documents/Scratch\ Projects
 git clone https://github.com/clanfieldCEP/CodeClub.git .
 
-#mv ~/Documents/Scratch\ Projects/scratch14/ChristmasResources/ /usr/share/scratch/Media/ChristmasResources/
-#mv ~/Documents/Scratch\ Projects/scratch14/resources/ /usr/share/scatch/Media/Resources/
+mv ~/Documents/Scratch\ Projects/scratch14/resources/ChristmasResources/ /usr/share/scratch/Media/ChristmasResources/
+mv ~/Documents/Scratch\ Projects/scratch14/resources/ /usr/share/scratch/Media/Resources/
+
+mv /usr/share/scratch/Media/ChristmasResources/ChristmasResources/* /usr/share/scratch/Media/ChristmasResources/.
+mv /usr/share/scratch/Media/Resources/resources/* /usr/share/scratch/Media/Resources/.
+
+sudo rmdir /usr/share/scratch/Media/ChristmasResources/ChristmasResources/
+sudo rmdir /usr/share/scratch/Media/Resources/resources/
